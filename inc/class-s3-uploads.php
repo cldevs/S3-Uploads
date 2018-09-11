c143e6c0e0494c59e1b7661eae92dba6a28a92bd
Atkins <atkinpau@corp.advancedsolutions.eds.com>
2018-09-11 11:42:22 -0700

    bugfix

--- inc/class-s3-uploads.php
+++ inc/class-s3-uploads.php
@@ -91,8 +91,12 @@ class S3_Uploads {
 		$this->original_upload_dir = $dirs;
 
 		$dirs['path']    = str_replace( WP_CONTENT_DIR, 's3://' . $this->bucket, $dirs['path'] );
-		$dirs['basedir'] = str_replace( WP_CONTENT_DIR, 's3://' . $this->bucket, $this->bucket_dir.$dirs['basedir'] );
-
+		$dirs['basedir'] = str_replace( WP_CONTENT_DIR, 's3://' . $this->bucket, $dirs['basedir'] );
+		echo "=".$dirs['basedir']."=\n";
+		echo "=".$dirs['path']."=\n";
+		echo "=".$dirs['WP_CONTENT_DIR']."=\n";
+		echo "=".'s3://' . $this->bucket."=\n";
+		echo "=".$this->get_s3_url()."=\n";
 		if ( ! defined( 'S3_UPLOADS_DISABLE_REPLACE_UPLOAD_URL' ) || ! S3_UPLOADS_DISABLE_REPLACE_UPLOAD_URL ) {
 
 			if ( defined( 'S3_UPLOADS_USE_LOCAL' ) && S3_UPLOADS_USE_LOCAL ) {
